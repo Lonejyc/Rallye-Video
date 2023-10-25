9c834572e463e6cceb626f91537b5d5248d00944
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2023-10-25 10:13:51 +0200

    C'est tchao

--- equipes.php
+++ equipes.php
@@ -1,13 +1,9 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
-    <?php
-        require_once('connexion.php');
-        ?>
+    <?php require_once('connexion.php') ?>
+
     <link href='css/style.css' rel='stylesheet'>
 
     <meta charset="UTF-8">
@@ -17,18 +13,7 @@
 </head>
 
 <body>
-    <header>
-        <a href="index.php"><h1>Rallye video - Équipe</h1></a>
-        <img src="images/logo.svg">
-        <ul>
-            <li><h2>Événement</h2></li>
-            <li><a href="equipes.php"><h2>Équipe</h2></a></li>
-            <li><h2>Dépot</h2></li>
-            <li><a href="vote.php"><h2>Vote</h2></a></li>
-        </ul>
-        <h2>Live</h2>
-        <a href="inscription.php"><h2>Connexion</h2></a>
-    </header>
+    <?php include("Global/header.html") ?>
     <main>
         <div class="Liste">
             <p>Listes des équipes</p>
@@ -50,62 +35,57 @@
             ?>
         </div>
         <div class="Inscription">
-            <p>Inscription dans une équipe</p>
-            <form action="traitement.php" method="post">
-                <p>Nom de l’équipe :</p>
-                <select name="categorie" id="input-cat" value="Etat" required>
-                        <option value="1">Ouai ouai</option>
-                    </select>
-                </div>
-                <br>
-                <input type="submit" value="OK">
-                <p>L'équipe n'existe pas encore ? Inscris-la maintenant !</p>
-            </form>
-            <p>Inscription d'une équipe</p>
-            <form action="traitement.php" method="post">
-                <p>Nom de l’équipe :</p>
-                <div class="equipe">
-                    <input type="text" name="nom_equipe" placeholder="Exemple : Les zigotos" required>
-                </div>
-                <p>Membres de l’équipe :</p>
-                <div class="membre">
-                    <input type="text" name="nom_membre" placeholder="Exemple : Jean Dupont" required>
-                </div>
-                <br>
-                <button type="button" id="ajouter_membre">Ajouter un membre</button>
-                <br>
-                <input type="submit" value="OK">
-                <p>L'équipe existe déjà ? Si oui, joins-toi à elle !</p>
-            </form>
-
-             <!-- Script pour ajouter un membre
-            <script>
-                document.getElementById('ajouter_membre').addEventListener('click', function() {
-                    var membresDiv = document.getElementById('membres');
-                    var newMembreDiv = document.querySelector('.membre').cloneNode(true);
-                    membresDiv.appendChild(newMembreDiv);
-                });
-            </script>
-             -->
+            <div>
+                <p>Inscription dans une équipe</p>
+                <form method="POST">
+                    <?php
+                    $request = "SELECT * FROM rv_team";
+                    $noms = mysqli_query($CONNEXION, $request);
+                    ?>
+                    <div id="equipe-select">
+                        <select id="id" required>
+                            <option value="" disabled selected></option>
+                            <?php while ($nom = mysqli_fetch_assoc($noms)): ?>
+                                <option value="<?php echo $nom['id'] ?>"><?php echo $nom['Nom_equipe']; ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
+                    <br>
+                    <button type="submit" name="action" value="confirmation">OK</button>
+                    <p>L'équipe n'existe pas encore ? Inscris-la maintenant !</p>
+                </form>
+            </div>
+            <div>
+                <p>Inscription d'une équipe</p>
+                <form action="traitement.php" method="post">
+                    <p>Nom de l’équipe :</p>
+                    <div class="equipe">
+                        <input type="text" name="nom_equipe" placeholder="Exemple : Les zigotos" required>
+                    </div>
+                    <p>Membres de l’équipe :</p>
+                    <div class="membre">
+                        <input type="text" name="nom_membre" placeholder="Exemple : Jean Dupont" required>
+                        <input type="text" name="nom_membre2" placeholder="Exemple : Jean Duval" required>
+                        <input type="text" name="nom_membre3" placeholder="Exemple : Jean Aval" required>
+                        <input type="text" name="nom_membre4" placeholder="Exemple : Jean Anal" required>
+                    </div>
+                    <br>
+                    <button type="button" id="ajouter_membre">Ajouter un membre</button>
+                    <br>
+                    <button type="submit" name="action" value="confirmation">OK</button>
+                    <p>L'équipe existe déjà ? Si oui, joins-toi à elle !</p>
+                </form>
+                <script>
+                    document.getElementById('ajouter_membre').addEventListener('click', function() {
+                        var membresDiv = document.getElementById('membres');
+                        var newMembreDiv = document.querySelector('.membre').cloneNode(true);
+                        membresDiv.appendChild(newMembreDiv);
+                    });
+                </script>
+            </div>
         </div>
     </main>
-    <footer>
-        <div>
-            <ul>
-                <li>
-                    <h3>PARTENAIRES</h3>
-                </li>
-                <li>
-                    <h3>MENTIONS LÉGALES</h3>
-                </li>
-                <li>
-                    <h3>À PROPOS</h3>
-                </li>
-            </ul>
-            <img src="images/fleche.svg">
-        </div>
-        <p>Copyright © Rallye Vidéo MMI 2023-2024</p>
-    </footer>
+    <?php include("Global/footer.html") ?>
 </body>
 
 </html>
