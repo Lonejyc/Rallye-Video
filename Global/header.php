4f5391cf1b1ad6da1385e5c041bb0933c447e3f6
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2023-11-15 08:23:04 +0100

    update header

--- Global/header.php
+++ Global/header.php
@@ -3,18 +3,18 @@
         <div class="logo">
             <a href="index.php"><img src="images/logo_cam.svg"><p>Rallye video</p></a>
         </div>
-        <ul>
+        <ul class="mid">
             <li><a href="evenement.php">Événement</a></li>
             <li><a href="equipes.php">Équipe</a></li>
             <li><a href="depot.php">Dépot</a></li>
             <li><a href="vote.php">Vote</a></li>
         </ul>
-        <ul>
-            <li><a href="live.php">Live</a></li>
+        <ul class="right">
+            <li id="live"><a href="live.php">Live</a></li>
             <?php if (isset($_SESSION['user_id'])) {
-                echo "<li><a href='dashboard.php'>Dashboard</a></li>";
+                echo "<li class='registration'><a href='dashboard.php'>Dashboard</a></li>";
             } else {
-                echo "<li><a href='se_connecter.php'>Connexion</a></li>";
+                echo "<li class='registration'><a href='se_connecter.php'>Connexion</a></li>";
             } ?>
         </ul>
     </nav>
