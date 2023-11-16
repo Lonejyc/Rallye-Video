fb302d05f136be5253f25c54ad9c3f499400de34
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2023-11-16 22:09:29 +0100

    remarchage de la page live

--- live.php
+++ live.php
@@ -34,7 +34,63 @@
                 <div id="player"></div>
                 <div id="live-chat"></div>
 
-                <script src="js/live.min.js"></script>
+                <script>
+                    // Variables pour stocker l'ID de la vidéo YouTube et la clé API
+                    const videoId = 'vMRXc06uN2vmpwWU'; // Remplacez par l'ID de votre vidéo
+                    const apiKey = 'AIzaSyD03L3Cvbq--FipuDu-u5uLvgbPRcwFDBk'; // Remplacez par votre clé API
+
+                    // Fonction d'initialisation du lecteur vidéo
+                    function onYouTubeIframeAPIReady() {
+                        new YT.Player('player', {
+                            height: '360',
+                            width: '640',
+                            videoId: videoId,
+                            events: {
+                                'onReady': onPlayerReady,
+                            }
+                        });
+                    }
+
+                    // Fonction appelée lorsque le lecteur est prêt
+                    function onPlayerReady(event) {
+                        event.target.playVideo();
+                        // Chargez les commentaires lorsque le lecteur est prêt
+                        loadComments();
+                        // Actualisez les commentaires toutes les 5 secondes (ajustable selon vos besoins)
+                        setInterval(loadComments, 5000);
+                    }
+
+                    // Fonction pour charger les commentaires
+                    function loadComments() {
+                        // Utilisez l'API YouTube Data v3 pour récupérer les commentaires
+                        $.get(
+                            'https://www.googleapis.com/youtube/v3/commentThreads',
+                            {
+                                part: 'snippet',
+                                videoId: videoId,
+                                key: apiKey,
+                                maxResults: 10,
+                            },
+                            function(data) {
+                                displayComments(data.items);
+                            }
+                        );
+                    }
+
+                    // Fonction pour afficher les commentaires
+                    function displayComments(comments) {
+                        var commentSection = $('#live-chat');
+                        commentSection.empty(); // Efface les commentaires existants
+
+                        comments.forEach(function(comment) {
+                            var commentText = comment.snippet.topLevelComment.snippet.textDisplay;
+                            commentSection.append('<p>' + commentText + '</p>');
+                        });
+
+                        // Ajoutez un formulaire pour ajouter des commentaires (si nécessaire)
+                        commentSection.append('<form id="comment-form"><textarea id="new-comment"></textarea><br/><button type="button" onclick="postComment()">Ajouter un commentaire</button></form>');
+                    }
+                </script>
             </div>
         </main>
         <?php include("global/footer.php") ?>
